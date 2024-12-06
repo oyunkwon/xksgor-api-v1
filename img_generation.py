43b407af586ec48a2cd586cc6df802cb1b3fc4f4
oyunkwon <22-007@ksa.hs.kr>
2024-12-07 07:20:36 +0900

    first commit

---
+++ img_generation.py
@@ -0,0 +1,21 @@
+from PIL import Image, ImageFont, ImageDraw
+
+# 이미지 로드
+def generate_image(name, num):
+    target_img = Image.open("static/template.png")
+
+    def loadfont(fontsize=50):
+    # ttf 파일의 경로를 지정
+        ttf = 'static/Pretendard-Bold.ttf'
+        return ImageFont.truetype(font=ttf, size=fontsize)
+
+
+    namefontObj = loadfont(fontsize=120)
+    numfontObj = loadfont(fontsize=83)
+    out_img = ImageDraw.Draw(target_img)
+    numStr = f"#{num:,}"
+    num_bbox = out_img.textbbox((0, 0), numStr, font=numfontObj)
+    text_width, text_height = num_bbox[2] - num_bbox[0], num_bbox[3] - num_bbox[1]
+    out_img.text(xy=(214, 220), text=name, fill=(255, 255, 255), font=namefontObj)
+    out_img.text(xy=(858 - text_width, 1562), text=numStr, fill=(255, 81, 0), font=numfontObj)
+    return target_img
---
+++ main.py
@@ -0,0 +1,24 @@
+from fastapi import FastAPI
+from fastapi.responses import StreamingResponse
+from io import BytesIO
+from img_generation import generate_image
+
+app = FastAPI()
+
+@app.get("/generate_image/")
+async def generate_image_endpoint(name: str, num: int):
+    # 이미지 생성
+    img = generate_image(name, num)
+
+    # 이미지를 메모리에서 바로 반환할 수 있도록 BytesIO로 변환
+    img_io = BytesIO()
+    img.save(img_io, 'PNG')
+    img_io.seek(0)
+
+    # 이미지 반환
+    return StreamingResponse(img_io, media_type="image/png")
+
+@app.get("/test")
+async def main():
+    # 이미지 생성
+    return {"message": "Hello World!"}
