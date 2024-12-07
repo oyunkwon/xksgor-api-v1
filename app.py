a030c62b433401c6a4b506e89356b2b1eaa23f13
oyunkwon <22-007@ksa.hs.kr>
2024-12-07 13:12:52 +0900

    cors

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from fastapi.responses import StreamingResponse
 from io import BytesIO
-from img_generation import generate_image
+from img_generation import generate_image, generate_image_twitter
 from mangum import Mangum
 from fastapi.middleware.cors import CORSMiddleware
 
@@ -17,6 +17,20 @@ async def generate_image_endpoint(name: str, num: str):
     # 이미지 생성
     num = int(num)
     img = generate_image(name, num)
+
+    # 이미지를 메모리에서 바로 반환할 수 있도록 BytesIO로 변환
+    img_io = BytesIO()
+    img.save(img_io, 'PNG')
+    img_io.seek(0)
+
+    # 이미지 반환
+    return StreamingResponse(img_io, media_type="image/png")
+
+@app.get("/generate_image/twitter/")
+async def generate_image_endpoint_twitter(name: str, num: str):
+    # 이미지 생성
+    num = int(num)
+    img = generate_image_twitter(name, num)
 
     # 이미지를 메모리에서 바로 반환할 수 있도록 BytesIO로 변환
     img_io = BytesIO()
--- img_generation.py
+++ img_generation.py
@@ -19,3 +19,23 @@ def generate_image(name, num):
     out_img.text(xy=(214, 220), text=name, fill=(255, 255, 255), font=namefontObj)
     out_img.text(xy=(858 - text_width, 1562), text=numStr, fill=(255, 81, 0), font=numfontObj)
     return target_img
+
+
+def generate_image_twitter(name, num):
+    target_img = Image.open("static/template-twitter.png")
+
+    def loadfont(fontsize=50):
+        # ttf 파일의 경로를 지정
+        ttf = 'static/Pretendard-Bold.ttf'
+        return ImageFont.truetype(font=ttf, size=fontsize)
+
+
+    namefontObj = loadfont(fontsize=120)
+    numfontObj = loadfont(fontsize=83)
+    out_img = ImageDraw.Draw(target_img)
+    numStr = f"#{num:,}"
+    num_bbox = out_img.textbbox((0, 0), numStr, font=numfontObj)
+    text_width, text_height = num_bbox[2] - num_bbox[0], num_bbox[3] - num_bbox[1]
+    out_img.text(xy=(160, 60), text=name, fill=(255, 255, 255), font=namefontObj)
+    out_img.text(xy=(1020 - text_width, 400), text=numStr, fill=(255, 81, 0), font=numfontObj)
+    return target_img
